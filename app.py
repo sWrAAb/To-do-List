38d9525c093b0d3bbfed34c6a6e9f4a13eb09756
sWrAAb <swraab@gmail.com>
2024-02-04 23:36:20 +0000

    Create new task completed

--- app.py
+++ app.py
@@ -1,24 +1,17 @@
 import os
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'to-do.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'todo.db')
 db = SQLAlchemy(app)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/To-do-List', methods=['POST', 'GET'])
-def TodoList():
-    return render_template('To-do-List.html')
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default = 0)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -35,6 +28,27 @@ def create_app():
     return app
 
 create_app()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/To-do-List', methods=['POST', 'GET'])
+def TodoList():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/To-do-List')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('To-do-List.html', tasks=tasks)
 
 
 if __name__ == '__main__':
